5fe1782e033b29de1ce2641c2fc640984d3b7bc4
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2022-04-25 18:23:46 +0300

    Migrated sensitive info to environment variables

--- bumbu/settings.py
+++ bumbu/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# load .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ah#65%4acbt61-c$z$n!51fmdx655uh-1ip3#qr=k79!7yfjh3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
